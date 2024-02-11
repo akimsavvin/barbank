0ec0c8cd43a8b6c5dbef74983d6b63fc5f5f2dcc
Cenken00 <x.kadzitsu.x@gmail.com>
2024-02-11 19:12:01 +0300

    created data context

---
+++ BarBank/Common/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+using System.Data;
+
+
+namespace BarBank.Common.Data;
+
+public class DataContext
+{
+    private readonly IConfiguration _configuration;
+    public DataContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public IDbConnection CreateConnection()
+    {
+        var connectionString = _configuration.GetValue<string>("Database:ConnectionString")!;
+        return new NpgsqlConnection(connectionString);
+    }
+}
