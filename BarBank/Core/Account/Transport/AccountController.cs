3c0981dc8b80c265e7fe9dee28d17f2c9307ef75
Cenken00 <x.kadzitsu.x@gmail.com>
2024-03-14 21:59:17 +0300

    added create account http method

---
+++ BarBank/Core/Account/Transport/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using BarBank.Core.Account.Services;
+using BarBank.Core.Auth.Dto;
+using BarBank.Core.Auth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarBank.Core.Account.Transport;
+
+[Route("/api/accounts")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IBankAccountService _bankAccountService;
+    private readonly IJwtService _jwtService;
+
+    public AccountController(IBankAccountService bankAccountService, IJwtService jwtService)
+    {
+        _bankAccountService = bankAccountService;
+        _jwtService = jwtService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromHeader(Name = "Authorization")] string authHeader)
+    {
+        var jwt = authHeader.Split(' ')[1];
+        var userId = _jwtService.GetSubFromJwt(jwt);
+        var account = await _bankAccountService.CreateAsync(userId);
+
+        return Created("/api/accounts", account);
+    }
+}
